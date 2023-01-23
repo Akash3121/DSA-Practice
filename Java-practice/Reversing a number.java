b73968707de48859e71b88372ba4c7159d431945
Akash Reddy Jammula <jammulaakashreddy03@gmail.com>
2023-01-23 12:32:26 +0530

    Create Reversing a number.java

---
+++ Java-practice/Reversing a number.java
@@ -0,0 +1,16 @@
+//reversing a number
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+	    int n = in.nextInt();
+	    int ans=0;
+	    while (n>0){
+	        int rem = n%10;
+	        ans = ans *10 + rem;
+	        n/= 10;
+	    }
+	    System.out.println(ans);
+	}
+}
