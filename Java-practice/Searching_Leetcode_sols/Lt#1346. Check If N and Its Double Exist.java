278a792c6ba0b403ec2d5ae34af7100d4069a1fb
Akash Reddy Jammula <jammulaakashreddy03@gmail.com>
2023-02-04 16:49:32 +0530

    Update Lt#1346. Check If N and Its Double Exist.java

--- Java-practice/Searching_Leetcode_sols/Lt#1346. Check If N and Its Double Exist.java
+++ Java-practice/Searching_Leetcode_sols/Lt#1346. Check If N and Its Double Exist.java
@@ -28,3 +28,22 @@ class Solution {
     return false;
     }
 }
+
+//Javascript
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var checkIfExist = function(arr) {
+    let mul = {};
+    let res = false;
+
+    for(let i = 0; i< arr.length; i++ ) {
+        if(arr[i]*2 in mul || arr[i]/2 in mul) {
+            res = true;
+            break;
+        }
+        mul[arr[i]] = i;
+    }
+    return res;
+};
