0534a30b9c51855c7f5e908d91f84bc1efd336fb
Akash Reddy Jammula <jammulaakashreddy03@gmail.com>
2023-02-11 12:56:08 +0530

    Update Lt#287. Find the Duplicate Number.java

--- Java-practice/Searching_Leetcode_sols/Lt#287. Find the Duplicate Number.java
+++ Java-practice/Searching_Leetcode_sols/Lt#287. Find the Duplicate Number.java
@@ -11,3 +11,40 @@ class Solution {
         return -1;
     }
 }
+
+//binary search 
+//time- O(nlogn)
+//space- O(1)
+class Solution {
+    public int findDuplicate(int[] nums) {
+//         According to the Pigeonhole Principle, n+1 integers, placed in an array of length n, at least 1 integer will be repeated.
+
+// So guess a number first(the number mid in the valid range[left,right]), count the elements of the array which is less than or equal to mid in the array.
+
+// If cnt is strictly greater than mid. According to the Pigeonhole Principle, repeated elements are in the interval [left,mid];
+// Otherwise, the repeated element is in the interval [mid+1,right].
+// With extra O(1) space, without modifying the input.
+
+    int len = nums.length;
+    int low = 1;
+    int high = len - 1;
+
+    while(low < high) { //logn times binary search
+        int mid = low + (high - low) / 2;
+        int count = 0;
+
+        for (int i = 0; i < len; i++){ //n times linear loop
+            if (nums[i] <= mid){
+                count++;
+            }
+        }
+
+        if (count <= mid){
+            low = mid+ 1;
+        } else {//strictly greater, then repeated elem in left-mid so high = mid
+            high = mid;
+        }
+    }
+    return low; //here low == high
+    }
+}
