b17f8c886a34a7155b3462247ce35a313a6e62a6
Akash Reddy Jammula <jammulaakashreddy03@gmail.com>
2023-02-12 11:39:14 +0530

    Update Lt#15. 3Sum.java

--- Java-practice/Searching_Leetcode_sols/Lt#15. 3Sum.java
+++ Java-practice/Searching_Leetcode_sols/Lt#15. 3Sum.java
@@ -26,3 +26,34 @@ class Solution:
                     while (nums[l] == nums[l - 1] and l<r): #l<r because we incremented l+1, there might be a case l exceeds r
                         l += 1
         return res
+
+//Java
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Set<List<Integer>> res = new HashSet<>();
+
+        if (nums.length < 3){ //not required in this problem - see constraints
+            return new ArrayList<>(res);
+        }
+
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length -2; i++){
+            int l = i + 1;
+            int r = nums.length - 1;
+            while(l < r){
+                int sum = nums[i] + nums[l] + nums[r];
+                if (sum > 0){
+                    r--;
+                } 
+                else if (sum < 0){
+                    l++;
+                }
+                else{ //sum === 0 case
+                    res.add(Arrays.asList(nums[i], nums[l++], nums[r--]));
+                }
+            }
+        }
+        return new ArrayList<>(res);
+    }
+}
