577a3a07cb10e4c55f238cb7d4a40c3642563f3c
Akash Reddy Jammula <jammulaakashreddy03@gmail.com>
2023-02-15 13:28:30 +0530

    Update Lt#153. Find Minimum in Rotated Sorted Array.java

--- Java-practice/Searching_Leetcode_sols/Lt#153. Find Minimum in Rotated Sorted Array.java
+++ Java-practice/Searching_Leetcode_sols/Lt#153. Find Minimum in Rotated Sorted Array.java
@@ -35,3 +35,24 @@ class Solution {
         return -1; // it will not come here because the array is unique and it must have a pivot.
     }
 }
+
+//python
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        lo = 0
+        hi = len(nums) - 1
+
+        # if not sorted
+        if nums[lo] < nums[hi] : 
+            return nums[lo]
+
+        while(lo < hi):
+            mid = lo + (hi - lo) // 2
+
+            if (nums[mid] > nums[hi]):
+                lo = mid + 1
+            elif (nums[mid] < nums[hi]):
+                hi = mid
+            else: #all equal 
+                hi -= 1 #reducing the duplicates
+        return nums[lo]
