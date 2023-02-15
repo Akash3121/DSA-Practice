9e203bb18b86185a18361cb92c760f389ed73b03
Akash Reddy Jammula <jammulaakashreddy03@gmail.com>
2023-02-15 15:16:29 +0530

    Update Lt#154. Find Minimum in Rotated Sorted Array II.java

--- Java-practice/Searching_Leetcode_sols/Lt#154. Find Minimum in Rotated Sorted Array II.java
+++ Java-practice/Searching_Leetcode_sols/Lt#154. Find Minimum in Rotated Sorted Array II.java
@@ -20,3 +20,31 @@ class Solution:
             else: #all equal 
                 hi -= 1 #reducing the duplicates
         return nums[lo]
+
+//javascript
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMin = function(nums) {
+    let lo = 0;
+    let hi = nums.length - 1;
+
+    if (nums[lo] < nums[hi]){
+        return nums[lo];
+    } 
+
+    while (lo < hi){
+        let mid = lo + Math.floor((hi - lo)/2) ;
+
+        if (nums[mid] > nums[hi]){
+            lo = mid + 1;
+        }
+        else if (nums[mid] < nums[hi]){
+            hi = mid;
+        } else { //eliminating duplicates
+            hi --; 
+        }
+    }
+    return nums[lo];
+};
