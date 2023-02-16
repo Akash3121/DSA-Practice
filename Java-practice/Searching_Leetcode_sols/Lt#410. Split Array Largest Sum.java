94a390fec06268f6f75499c64a2dbb11bd9d6645
Akash Reddy Jammula <jammulaakashreddy03@gmail.com>
2023-02-16 13:17:28 +0530

    Update Lt#410. Split Array Largest Sum.java

--- Java-practice/Searching_Leetcode_sols/Lt#410. Split Array Largest Sum.java
+++ Java-practice/Searching_Leetcode_sols/Lt#410. Split Array Largest Sum.java
@@ -44,3 +44,32 @@ public class SplitArray {
         return end; //here start == end
     }
 }
+
+//python
+class Solution:
+    def splitArray(self, nums: List[int], k: int) -> int:
+        def canSplit(largest):
+            subArray = 0
+            currSum = 0
+            for n in nums:
+                currSum += n
+                if currSum >largest:
+                    subArray += 1
+                    currSum = n
+            return subArray + 1 <= k # + 1 is for the last sub array
+
+
+
+        l, r = max(nums), sum(nums)
+        res = r
+        while l<= r: #logn times
+            mid = l + ((r-l) // 2)
+
+            if canSplit(mid): #n times
+                res = mid
+                r = mid - 1
+            else:
+                l = mid + 1
+        return res
+
+                
