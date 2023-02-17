868bede8424c7ff75833c642fd7ab220aed9ad58
Akash Reddy Jammula <jammulaakashreddy03@gmail.com>
2023-02-17 20:24:38 +0530

    Update BubbleSort.java

--- Algorithms/BubbleSort.java
+++ Algorithms/BubbleSort.java
@@ -1,3 +1,5 @@
 //also known as Siniking sort, Exchange sort
 //swapping with the next adjacent element if first elem is greater than second elem
 //after first pass greatest elem is at end
+//In second pass second largest element is from last to second place 
+// It's like sorting from last to first
