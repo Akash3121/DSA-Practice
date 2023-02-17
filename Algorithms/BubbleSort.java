ffbe6b7699a0305b6978118f2b769cdc283cde31
Akash Reddy Jammula <jammulaakashreddy03@gmail.com>
2023-02-17 20:31:57 +0530

    Update BubbleSort.java

--- Algorithms/BubbleSort.java
+++ Algorithms/BubbleSort.java
@@ -3,3 +3,6 @@
 //after first pass greatest elem is at end
 //In second pass second largest element is from last to second place 
 // It's like sorting from last to first
+// As for each iteration last elements are sorted, so it doesn't matter if we compare those last sorted elements in j counter
+// so to tackle this, we go till j - i, so that we reduce some un necessary comparisions
+
