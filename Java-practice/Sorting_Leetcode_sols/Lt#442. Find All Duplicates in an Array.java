b7ee6fc62e22a5a24a725d0cdcc2b49d2d71e6a0
Akash Reddy Jammula <jammulaakashreddy03@gmail.com>
2023-02-20 21:17:35 +0530

    Create Lt#442. Find All Duplicates in an Array.java

---
+++ Java-practice/Sorting_Leetcode_sols/Lt#442. Find All Duplicates in an Array.java
@@ -0,0 +1,30 @@
+//442. Find All Duplicates in an Array
+
+class Solution {
+    public List<Integer> findDuplicates(int[] nums) {
+        // Logic: use cyclic sort as 1 to N and then return elements which are not in their correct position
+        int i = 0;
+        while (i < nums.length){
+            int correct = nums[i] - 1; //index = val - 1
+            if (i != correct &&  nums[correct] != nums[i]){
+                // swap
+                int temp = nums[i];
+                nums[i] = nums[correct];
+                nums[correct] = temp;
+            }
+            else {
+                i++;
+            }
+        }
+        
+        //loop the array and then which are not at correct index are appeared twice
+        List<Integer> ans = new ArrayList<>();
+        for(i = 0; i < nums.length; i++){
+            if (i != nums[i] - 1){
+                ans.add(nums[i]);
+            }
+        }
+
+        return ans;
+    }
+}
