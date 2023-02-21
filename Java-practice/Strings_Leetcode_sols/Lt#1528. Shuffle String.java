cecfe811934b26948bbd6a445996dd125bd3dd55
Akash Reddy Jammula <jammulaakashreddy03@gmail.com>
2023-02-21 13:22:23 +0530

    Create Lt#1528. Shuffle String.java

---
+++ Java-practice/Strings_Leetcode_sols/Lt#1528. Shuffle String.java
@@ -0,0 +1,9 @@
+//1528. Shuffle String
+
+#python
+class Solution:
+    def restoreString(self, s: str, indices: List[int]) -> str:
+        res = [''] * len(s)
+        for i, ch in enumerate(s):
+            res[indices[i]] = ch
+        return "".join(res)
