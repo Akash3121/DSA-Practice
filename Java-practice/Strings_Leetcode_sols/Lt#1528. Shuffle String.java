41f60c0babdf1c49417e3583d96b50ee9ceff38d
Akash Reddy Jammula <jammulaakashreddy03@gmail.com>
2023-02-21 13:53:11 +0530

    Update Lt#1528. Shuffle String.java

--- Java-practice/Strings_Leetcode_sols/Lt#1528. Shuffle String.java
+++ Java-practice/Strings_Leetcode_sols/Lt#1528. Shuffle String.java
@@ -7,3 +7,30 @@ class Solution:
         for i, ch in enumerate(s):
             res[indices[i]] = ch
         return "".join(res)
+//Java
+class Solution {
+    public String restoreString(String s, int[] indices) {
+		char temp[] = new char[indices.length];
+		for (int i = 0; i < indices.length; i++) {
+			temp[indices[i]] = s.charAt(i);
+		}
+		return new String(temp);
+	}
+}
+
+//javascript
+/**
+ * @param {string} s
+ * @param {number[]} indices
+ * @return {string}
+ */
+var restoreString = function(s, indices) {
+    let ans = new Array(indices.length);
+
+    for(let i = 0; i < indices.length; i++){
+        ans[indices[i]] = s[i];
+    }
+
+    return ans.join("");
+    
+};
