429bc02d2afca502a7de509b9d890f73b497718e
Akash Reddy Jammula <jammulaakashreddy03@gmail.com>
2023-02-22 17:32:13 +0530

    Update Lt#1967. Number of Strings That Appear as Substrings in Word.java

--- Java-practice/Strings_Leetcode_sols/Lt#1967. Number of Strings That Appear as Substrings in Word.java
+++ Java-practice/Strings_Leetcode_sols/Lt#1967. Number of Strings That Appear as Substrings in Word.java
@@ -12,3 +12,9 @@ class Solution {
         return k;
     }
 }
+
+#python
+class Solution:
+    def numOfStrings(self, patterns: List[str], word: str) -> int:
+        return sum(p in word for p in patterns)
+        
