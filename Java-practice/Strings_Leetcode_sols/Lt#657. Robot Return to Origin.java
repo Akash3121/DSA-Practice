668c063f2bce8760b386966be853a2c63eab0c72
Akash Reddy Jammula <jammulaakashreddy03@gmail.com>
2023-02-22 17:32:32 +0530

    Create Lt#657. Robot Return to Origin.java

---
+++ Java-practice/Strings_Leetcode_sols/Lt#657. Robot Return to Origin.java
@@ -0,0 +1,10 @@
+//657. Robot Return to Origin
+
+#python
+class Solution(object):
+    def judgeCircle(self, moves):
+        """
+        :type moves: str
+        :rtype: bool
+        """
+        return moves.count("U") == moves.count("D") and moves.count("L") == moves.count("R")
