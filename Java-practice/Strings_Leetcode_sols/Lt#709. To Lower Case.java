a206d57db1d17c5c6658bbc4e4d76799c9988286
Akash Reddy Jammula <jammulaakashreddy03@gmail.com>
2023-02-22 09:00:28 +0530

    Update Lt#709. To Lower Case.java

--- Java-practice/Strings_Leetcode_sols/Lt#709. To Lower Case.java
+++ Java-practice/Strings_Leetcode_sols/Lt#709. To Lower Case.java
@@ -3,4 +3,21 @@
 #python
 class Solution:
     def toLowerCase(self, s: str) -> str:
+        # return s.lower()
+        arr = list(s)
+        for i in range(len(arr)):
+            if ord(arr[i]) <= ord("Z") and ord(arr[i]) >= ord("A"):
+                arr[i] = chr(ord(arr[i]) + ord("a") - ord("A"))
+        return "".join(arr)
+
+#python
+class Solution:
+    def toLowerCase(self, s: str) -> str:
         return s.lower()
+
+//Java
+class Solution {
+    public String toLowerCase(String s) {
+        return s.toLowerCase();
+    }
+}
