8e17517fb1d1c6bf135f1ad0a561e4e9abb6e2f1
Akash Reddy Jammula <jammulaakashreddy03@gmail.com>
2023-02-22 17:32:47 +0530

    Update Lt#657. Robot Return to Origin.java

--- Java-practice/Strings_Leetcode_sols/Lt#657. Robot Return to Origin.java
+++ Java-practice/Strings_Leetcode_sols/Lt#657. Robot Return to Origin.java
@@ -8,3 +8,19 @@ class Solution(object):
         :rtype: bool
         """
         return moves.count("U") == moves.count("D") and moves.count("L") == moves.count("R")
+
+#python3
+class Solution:
+    def judgeCircle(self, moves: str) -> bool:
+        up = 0
+        left = 0
+        for i in range(len(moves)):
+            if moves[i] == "U" :
+                up += 1
+            elif moves[i] == "L":
+                left += 1
+            elif moves[i] == "D" :
+                up -= 1
+            elif moves[i] == "R":
+                left -= 1
+        return up == 0 and left == 0
