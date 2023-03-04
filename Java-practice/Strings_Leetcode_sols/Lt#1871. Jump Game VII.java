17757deb16a9b807a06581c36d0843acc47edc1d
Akash Reddy Jammula <jammulaakashreddy03@gmail.com>
2023-03-04 11:00:09 +0530

    Create Lt#1871. Jump Game VII.java

---
+++ Java-practice/Strings_Leetcode_sols/Lt#1871. Jump Game VII.java
@@ -0,0 +1,19 @@
+//1871. Jump Game VII
+
+//cpp
+class Solution {
+public:
+    bool canReach(string s, int minJump, int maxJump) {
+        //sliding window
+    int cnt = 0;
+    vector<bool> dp(s.size());
+    dp[0] = true;
+    for (int i = minJump; i < s.size(); ++i) {
+        cnt += dp[i - minJump];
+        if (i - maxJump > 0)
+            cnt -= dp[i - maxJump - 1];
+        dp[i] = s[i] == '0' && cnt;
+    }
+    return dp.back();
+}  
+};
