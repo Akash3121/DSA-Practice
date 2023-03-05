5c45ab3405d809b3be3f51a23c6e1ad5e905d4c1
Akash Reddy Jammula <jammulaakashreddy03@gmail.com>
2023-03-05 09:11:56 +0530

    Update 05032023Lt#1345. Jump Game IV.java

--- Leetcode-DailyChallenge/05032023Lt#1345. Jump Game IV.java
+++ Leetcode-DailyChallenge/05032023Lt#1345. Jump Game IV.java
@@ -1,5 +1,46 @@
 //1345. Jump Game IV
 
+//Java
+class Solution {
+    public int minJumps(int[] arr) {
+        int n = arr.length;
+        Map<Integer, List<Integer>> valIndex = new HashMap();
+        for (int i = 0; i < n; i++){
+            valIndex.computeIfAbsent(arr[i], l -> new ArrayList()).add(i);
+        }
+        
+        Queue<Integer> q = new LinkedList();
+        Set<Integer> visited = new HashSet();
+
+        q.add(0);
+        visited.add(0);
+        int steps = 0;
+
+        while (!q.isEmpty()){
+            int size = q.size();
+            while (size-- > 0){
+                int i = q.poll();
+                if (n-1 == i){
+                    return steps;
+                }
+                List<Integer> addList = valIndex.get(arr[i]);
+                addList.add(i-1);
+                addList.add(i+1);
+                for(int j: addList){
+                    if (j >= 0 && j < n && !visited.contains(j)){
+                        q.add(j);
+                        visited.add(j);
+                    }
+                } 
+                addList.clear(); 
+            }
+            steps ++;
+        }
+        return -1;
+    }
+}
+
+
 #python3
 class Solution:
     def minJumps(self, arr: List[int]) -> int:
