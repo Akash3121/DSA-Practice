1eefc23f29bdebbab3632de657b66321f62270a2
Akash Reddy Jammula <jammulaakashreddy03@gmail.com>
2023-03-07 18:37:27 +0530

    Create Lt#1813. Sentence Similarity III.java

---
+++ Java-practice/Strings_Leetcode_sols/Lt#1813. Sentence Similarity III.java
@@ -0,0 +1,24 @@
+//1813. Sentence Similarity III
+
+#python3
+class Solution:
+    def areSentencesSimilar(self, sentence1: str, sentence2: str) -> bool:
+        s1 = sentence1.split()
+        s2 = sentence2.split()
+
+        # let s1 lenght be smaller
+        if (len(s1) > len(s2)):
+            s1, s2 = s2, s1 #swap
+
+        while (s1):
+            # we are checking first and last elems because we can only insert from one side, cn't add from both sides 
+            if s1[0] == s2[0]:
+                s1.pop(0) #0 is needed as pop removes the last elem by default
+                s2.pop(0)
+            elif s1[-1] == s2[-1]:
+                s1.pop()
+                s2.pop()
+            else:
+                return False
+
+        return True
