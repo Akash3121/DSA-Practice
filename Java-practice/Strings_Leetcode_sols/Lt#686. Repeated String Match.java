9b2356da843e1dbd6d6a2367a6560166d28927c0
Akash Reddy Jammula <jammulaakashreddy03@gmail.com>
2023-03-08 08:40:27 +0530

    Update Lt#686. Repeated String Match.java

--- Java-practice/Strings_Leetcode_sols/Lt#686. Repeated String Match.java
+++ Java-practice/Strings_Leetcode_sols/Lt#686. Repeated String Match.java
@@ -7,4 +7,18 @@ class Solution:
         # we need to multiply ans{here ans = times} * a so that equal to b length (OR) ans+1 * a 
         # so there are 2 cases we need to check ans * a with b and ans+1 * a with b if both are not satisfied then -1
         times = -(-len(b) // len(a)) # same as times = int(math.ceil(float(len(B)) / len(A)))
+        # only 2 cases to check so 
+        for i in range(2): # only 0, 1
+            if b in (a * (times + i)):
+                return (times + i)
+        return -1
+        # in interview don't directly use this -> return times * (b in (a * times)) or (times + 1) * (b in (a * (times + 1))) or -1
+
+#python3
+class Solution:
+    def repeatedStringMatch(self, a: str, b: str) -> int:
+        # logic: for b to be inside a, the length of a should be atleast equal to b or 1 greater, means lower bound is ceil (len(b)/len(a)) 
+        # we need to multiply ans{here ans = times} * a so that equal to b length (OR) ans+1 * a 
+        # so there are 2 cases we need to check ans * a with b and ans+1 * a with b if both are not satisfied then -1
+        times = -(-len(b) // len(a)) # same as times = int(math.ceil(float(len(B)) / len(A)))
         return times * (b in (a * times)) or (times + 1) * (b in (a * (times + 1))) or -1
