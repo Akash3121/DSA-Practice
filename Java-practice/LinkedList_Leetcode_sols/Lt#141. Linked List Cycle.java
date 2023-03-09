7cced38e9864cfb8f42afa95b55504ac94e3ae76
Akash Reddy Jammula <jammulaakashreddy03@gmail.com>
2023-03-09 10:20:59 +0530

    Update Lt#141. Linked List Cycle.java

--- Java-practice/LinkedList_Leetcode_sols/Lt#141. Linked List Cycle.java
+++ Java-practice/LinkedList_Leetcode_sols/Lt#141. Linked List Cycle.java
@@ -55,3 +55,31 @@ public class Solution {
         return false;
     }
 }
+
+//Javascript
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {
+    let fast = head;
+    let slow = head
+
+    while (fast && fast.next){
+        slow = slow.next;
+        fast = fast.next.next;
+        if (slow == fast){
+            return true;
+        }
+    }
+    return false;
+};
+
