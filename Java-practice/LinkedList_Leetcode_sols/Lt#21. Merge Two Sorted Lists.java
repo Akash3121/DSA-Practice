d09934302dcb1c1c09e82bf2690d52ca3246baa3
Akash Reddy Jammula <jammulaakashreddy03@gmail.com>
2023-03-12 10:04:22 +0530

    Update Lt#21. Merge Two Sorted Lists.java

--- Java-practice/LinkedList_Leetcode_sols/Lt#21. Merge Two Sorted Lists.java
+++ Java-practice/LinkedList_Leetcode_sols/Lt#21. Merge Two Sorted Lists.java
@@ -31,3 +31,71 @@ class Solution {
 
     }
 }
+
+//Java 
+//another approach with O(N) space
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+       //Logic: creating a new LL and adding these into those
+       //cache is CS: O(N) for temp result node head 
+       ListNode head = new ListNode();
+       ListNode cur = head;
+
+       while( list1 != null && list2 != null){
+           if (list1.val <= list2.val){
+               cur.next = list1;
+               list1 = list1.next; 
+           }
+           else{
+               cur.next = list2;
+               list2 = list2.next;
+           }
+           cur = cur.next;
+       }
+       if (list1 != null){
+           cur.next = list1;
+       }
+       else{
+           cur.next = list2;
+       }
+
+       return head.next;
+    }
+}
+
+#python3
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode() #dummy is start
+        tail = dummy #tail is end
+
+        while list1 and list2:
+            if list1.val <= list2.val:
+                tail.next = list1
+                list1 = list1.next
+            else:
+                tail.next = list2
+                list2 = list2.next
+            tail = tail.next
+        
+        if list1:
+            tail.next = list1
+        else:
+            tail.next = list2
+
+        return dummy.next
