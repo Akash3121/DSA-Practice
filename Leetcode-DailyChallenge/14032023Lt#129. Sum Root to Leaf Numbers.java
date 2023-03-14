90a7d0d8e10b0609657ae4cc2586145482473f3c
Akash Reddy Jammula <jammulaakashreddy03@gmail.com>
2023-03-14 08:54:45 +0530

    Update 14032023Lt#129. Sum Root to Leaf Numbers.java

--- Leetcode-DailyChallenge/14032023Lt#129. Sum Root to Leaf Numbers.java
+++ Leetcode-DailyChallenge/14032023Lt#129. Sum Root to Leaf Numbers.java
@@ -22,3 +22,38 @@ class Solution:
             return sum
 
         return self.dfs(root.left, sum) + self.dfs(root.right, sum) 
+
+//Java
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int sumNumbers(TreeNode root) {
+        return dfs(root, 0);
+    }
+
+    public int dfs(TreeNode root, int sum){
+        if (root == null){
+            return 0;
+        }
+
+        sum = sum * 10 + root.val;
+
+        if (root.left == null && root.right == null){
+            return sum;
+        }
+        return dfs(root.left, sum) + dfs(root.right, sum);
+    }
+}
