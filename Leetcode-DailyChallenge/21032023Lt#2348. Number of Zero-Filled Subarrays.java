5b9e91c8fbff3bef7b554b9f3354830bff21ca50
Akash Reddy Jammula <jammulaakashreddy03@gmail.com>
2023-03-21 11:42:39 +0530

    Update 21032023Lt#2348. Number of Zero-Filled Subarrays.java

--- Leetcode-DailyChallenge/21032023Lt#2348. Number of Zero-Filled Subarrays.java
+++ Leetcode-DailyChallenge/21032023Lt#2348. Number of Zero-Filled Subarrays.java
@@ -27,3 +27,20 @@ func zeroFilledSubarray(nums []int) int64 {
     }
     return tot;
 }
+
+//Java
+class Solution {
+    public long zeroFilledSubarray(int[] nums) {
+        long res = 0;
+        long length = 0;
+        for(int num: nums){
+            if (num == 0){
+                res += ++length;
+            }
+            else{
+                length = 0;
+            }
+        }
+        return res;
+    }
+}
