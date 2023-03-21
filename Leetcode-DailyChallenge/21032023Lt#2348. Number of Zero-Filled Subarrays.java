c099d7fc8199554308ab2e63774bac2faa99f7b9
Akash Reddy Jammula <jammulaakashreddy03@gmail.com>
2023-03-21 11:36:07 +0530

    Update 21032023Lt#2348. Number of Zero-Filled Subarrays.java

--- Leetcode-DailyChallenge/21032023Lt#2348. Number of Zero-Filled Subarrays.java
+++ Leetcode-DailyChallenge/21032023Lt#2348. Number of Zero-Filled Subarrays.java
@@ -11,3 +11,19 @@ class Solution:
             else:
                 cur_sub_zeroes = 0
         return total_sub_zeroes
+
+//Golang
+func zeroFilledSubarray(nums []int) int64 {
+    var tot, cur int64 = 0, 0;
+    i := 0;
+    for i < len(nums){
+        if nums[i] == 0{
+            cur += 1
+            tot += cur
+        } else{
+            cur = 0
+        }
+        i++
+    }
+    return tot;
+}
