f75106728e10c6377889e764d272c51c20a97636
Akash Reddy Jammula <jammulaakashreddy03@gmail.com>
2023-03-28 13:06:18 +0530

    Update 28032023Lt#983. Minimum Cost For Tickets.java

--- Leetcode-DailyChallenge/28032023Lt#983. Minimum Cost For Tickets.java
+++ Leetcode-DailyChallenge/28032023Lt#983. Minimum Cost For Tickets.java
@@ -20,3 +20,21 @@ class Solution:
                 dp[i] = min(dp[i], c + dfs(j)) #j = i + d
             return dp[i]
         return dfs(0)
+
+#python3
+class Solution:
+    def mincostTickets(self, days: List[int], costs: List[int]) -> int:
+        dp = [0] * (days[-1] + 1)
+
+        travel_days = set(days) # for faster lookups
+
+        for i in range( 1, len(dp)):
+            if i in travel_days:
+                cost1 = dp[max(i - 1, 0)] + costs[0]
+                cost2 = dp[max(i - 7, 0)] + costs[1]
+                cost3 = dp[max(i - 30, 0)] + costs[2]
+                dp[i] = min(cost1, cost2, cost3) 
+            else:
+                dp[i] = dp[i - 1] #if not travelday then cost will be same as previous
+        
+        return dp[-1]
