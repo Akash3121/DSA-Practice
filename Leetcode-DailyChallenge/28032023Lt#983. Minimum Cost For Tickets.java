0fb25105a9e82591c7ee0442bff8360a8938f856
Akash Reddy Jammula <jammulaakashreddy03@gmail.com>
2023-03-28 12:54:58 +0530

    Create 28032023Lt#983. Minimum Cost For Tickets.java

---
+++ Leetcode-DailyChallenge/28032023Lt#983. Minimum Cost For Tickets.java
@@ -0,0 +1,22 @@
+//983. Minimum Cost For Tickets
+
+#python3
+class Solution:
+    def mincostTickets(self, days: List[int], costs: List[int]) -> int:
+        # TC: O(38 * N)
+        dp = {}
+
+        def dfs(i):
+            if i == len(days):
+                return 0
+            if i in dp:
+                return dp[i]
+
+            dp[i] = float("inf") # assigning
+            for d, c in zip([1, 7, 30], costs):
+                j = i
+                while j < len(days) and days[j] < days[i] + d:
+                    j += 1
+                dp[i] = min(dp[i], c + dfs(j)) #j = i + d
+            return dp[i]
+        return dfs(0)
