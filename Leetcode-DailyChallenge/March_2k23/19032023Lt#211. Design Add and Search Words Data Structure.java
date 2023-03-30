3d15c309afe9dbada1654deb4444e97636fb7fa0
Akash Reddy Jammula <jammulaakashreddy03@gmail.com>
2023-03-30 15:18:44 +0530

    Create 19032023Lt#211. Design Add and Search Words Data Structure.java

---
+++ Leetcode-DailyChallenge/March_2k23/19032023Lt#211. Design Add and Search Words Data Structure.java
@@ -0,0 +1,44 @@
+//211. Design Add and Search Words Data Structure
+
+#python3
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.endOfWith = False
+class WordDictionary:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def addWord(self, word: str) -> None:
+        cur = self.root
+
+        for c in word:
+            if c not in cur.children:
+                cur.children[c]=TrieNode()
+            cur = cur.children[c]
+        cur.endOfWith = True
+
+    def search(self, word: str) -> bool:
+        def dfs(j, root):
+            cur = root
+            for i in range(j, len(word)):
+                c = word[i]
+
+                if c == ".":
+                    for child in cur.children.values():
+                        if dfs(i+1, child):
+                            return True
+                    return False
+                else:
+                    if c not in cur.children:
+                        return False
+                    cur = cur.children[c]
+            return cur.endOfWith
+        return dfs(0, self.root)
+
+
+# Your WordDictionary object will be instantiated and called as such:
+# obj = WordDictionary()
+# obj.addWord(word)
+# param_2 = obj.search(word)
