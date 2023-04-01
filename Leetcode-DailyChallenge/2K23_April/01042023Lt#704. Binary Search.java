2dd679d34c5acc6e455cbf20d09092557b694f9c
Akash Reddy Jammula <jammulaakashreddy03@gmail.com>
2023-04-01 17:17:18 +0530

    Update 01042023Lt#704. Binary Search.java
    added Javascript code

--- Leetcode-DailyChallenge/2K23_April/01042023Lt#704. Binary Search.java
+++ Leetcode-DailyChallenge/2K23_April/01042023Lt#704. Binary Search.java
@@ -34,3 +34,27 @@ class Solution {
         
     }
 }
+
+//Javascript
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    let start = 0;
+    let end = nums.length -1;
+    while (start <= end){
+        let mid = start + Math.floor((end - start)/2);
+        if (nums[mid] < target){
+            start = mid + 1;
+        }
+        else if(nums[mid] > target){
+            end = mid - 1;
+        }
+        else{
+            return mid;
+        }
+    }
+    return -1;
+};
