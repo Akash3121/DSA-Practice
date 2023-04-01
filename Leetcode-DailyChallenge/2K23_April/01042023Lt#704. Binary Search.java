e57ae56fc621abd76e43690771d60c0d56dc86c1
Akash Reddy Jammula <jammulaakashreddy03@gmail.com>
2023-04-01 17:21:35 +0530

    Update 01042023Lt#704. Binary Search.java
    Added Go solution

--- Leetcode-DailyChallenge/2K23_April/01042023Lt#704. Binary Search.java
+++ Leetcode-DailyChallenge/2K23_April/01042023Lt#704. Binary Search.java
@@ -58,3 +58,20 @@ var search = function(nums, target) {
     }
     return -1;
 };
+
+// Go
+func search(nums []int, target int) int {
+    start := 0;
+    end := len(nums) - 1;
+    for (start <= end){
+        mid := start + (end - start) // 2;
+        if (nums[mid] < target){
+            start = mid + 1;
+        } else if nums[mid] > target {
+            end = mid - 1;
+        } else {
+            return mid;
+        }
+    }
+    return -1;
+}
