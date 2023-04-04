d915d80408e9bf0d11069e525e4b98248e8ffff6
Akash Reddy Jammula <jammulaakashreddy03@gmail.com>
2023-04-04 17:54:43 +0530

    Update 04042023Lt#2405. Optimal Partition of String.java

--- Leetcode-DailyChallenge/2K23_April/04042023Lt#2405. Optimal Partition of String.java
+++ Leetcode-DailyChallenge/2K23_April/04042023Lt#2405. Optimal Partition of String.java
@@ -16,3 +16,20 @@ class Solution {
         return ans;
     }
 }
+
+//Java
+class Solution {
+    public int partitionString(String s) {
+        Set<Character> set = new HashSet<>();
+        int res = 1;
+        for (int i = 0; i < s.length(); i++){
+            char ch = s.charAt(i);
+            if (!set.add(ch)){
+                res++;
+                set.clear();
+                set.add(ch);
+            }
+        } 
+        return res;
+    }
+}
