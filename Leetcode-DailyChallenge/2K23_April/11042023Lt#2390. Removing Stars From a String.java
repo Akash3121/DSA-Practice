d911e6d802cc16a98c16c3577e07b45fc04fb5c1
Akash Reddy Jammula <jammulaakashreddy03@gmail.com>
2023-04-11 13:59:45 +0530

    Update 11042023Lt#2390. Removing Stars From a String.java

--- Leetcode-DailyChallenge/2K23_April/11042023Lt#2390. Removing Stars From a String.java
+++ Leetcode-DailyChallenge/2K23_April/11042023Lt#2390. Removing Stars From a String.java
@@ -10,3 +10,18 @@ class Solution:
             else:
                 ans += [i]
         return "".join(ans)
+
+//Java
+class Solution {
+    public String removeStars(String s) {
+        StringBuilder c = new StringBuilder();
+        for(int i = 0; i < s.length(); i++){
+            if (s.charAt(i) == '*'){
+                c.deleteCharAt(c.length() - 1);
+            } else{
+                c.append(s.charAt(i));
+            }
+        }
+        return c.toString();
+    }
+}
