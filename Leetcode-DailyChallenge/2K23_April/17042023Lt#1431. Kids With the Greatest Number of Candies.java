714c6653a1d2ee169a92980fa64f26a98ed3eaae
Akash Reddy Jammula <jammulaakashreddy03@gmail.com>
2023-04-17 11:44:03 +0530

    Create 17042023Lt#1431. Kids With the Greatest Number of Candies.java

---
+++ Leetcode-DailyChallenge/2K23_April/17042023Lt#1431. Kids With the Greatest Number of Candies.java
@@ -0,0 +1,15 @@
+//1431. Kids With the Greatest Number of Candies
+//Java
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        List<Boolean> b = new ArrayList<Boolean>(candies.length);
+        int max = 0;
+        for (int i : candies){
+            max = Math.max(i,max);
+        }
+        for (int i : candies){
+            b.add(i+extraCandies >= max);
+        }
+        return b;
+    }
+}
