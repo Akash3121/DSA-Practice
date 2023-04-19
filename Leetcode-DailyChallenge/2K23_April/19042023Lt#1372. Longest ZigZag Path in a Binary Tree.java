99155bc13bc007849c11f049e2d44a4bf48feb34
Akash Reddy Jammula <jammulaakashreddy03@gmail.com>
2023-04-19 11:55:12 +0530

    Update 19042023Lt#1372. Longest ZigZag Path in a Binary Tree.java

--- Leetcode-DailyChallenge/2K23_April/19042023Lt#1372. Longest ZigZag Path in a Binary Tree.java
+++ Leetcode-DailyChallenge/2K23_April/19042023Lt#1372. Longest ZigZag Path in a Binary Tree.java
@@ -13,3 +13,41 @@ class Solution:
                 max(left_subtree[1] + 1, right_subtree[0] + 1, left_subtree[2], right_subtree[2])
             ]
         return dfs(root)[-1]
+
+// Java
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int longestZigZag(TreeNode root) {
+        int[] res =  dfs(root);
+        return res[2];
+    }
+
+    public int[] dfs(TreeNode node){
+        if (node == null){
+            return new int[]{-1, -1, -1};
+        }
+
+        int[] left_subtree = dfs(node.left);
+        int[] right_subtree = dfs(node.right);
+
+        int leftLength = left_subtree[1] + 1;
+        int rightLength = right_subtree[0] + 1;
+        int maxLength = Math.max(Math.max(leftLength, rightLength), Math.max(left_subtree[2], right_subtree[2]));
+
+        return new int[]{leftLength, rightLength, maxLength};
+    }
+}
