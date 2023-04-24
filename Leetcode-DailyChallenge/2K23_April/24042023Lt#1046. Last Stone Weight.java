a075c8b5a9353ab5cbae38734f76f264c66000cf
Akash Reddy Jammula <jammulaakashreddy03@gmail.com>
2023-04-24 11:36:11 +0530

    Update 24042023Lt#1046. Last Stone Weight.java

--- Leetcode-DailyChallenge/2K23_April/24042023Lt#1046. Last Stone Weight.java
+++ Leetcode-DailyChallenge/2K23_April/24042023Lt#1046. Last Stone Weight.java
@@ -14,3 +14,22 @@ class Solution:
         if len(stones) <1:
             return 0
         return stones[0]
+
+# python3 - optimized
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        
+        max_heap = [-stone for stone in stones]
+
+        heapq.heapify(max_heap)
+
+        while(len(max_heap) > 1):
+
+            stone1 = heapq.heappop(max_heap)
+            stone2 = heapq.heappop(max_heap)
+
+            if stone1 != stone2:
+                heapq.heappush(max_heap, stone1 - stone2)
+        if max_heap:
+            return -max_heap[0]
+        return 0
