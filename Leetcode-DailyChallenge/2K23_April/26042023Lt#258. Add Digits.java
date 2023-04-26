c13f50c11051ee7f95c0f6d061af928495aad0c3
Akash Reddy Jammula <jammulaakashreddy03@gmail.com>
2023-04-26 16:04:20 +0530

    Update 26042023Lt#258. Add Digits.java

--- Leetcode-DailyChallenge/2K23_April/26042023Lt#258. Add Digits.java
+++ Leetcode-DailyChallenge/2K23_April/26042023Lt#258. Add Digits.java
@@ -10,3 +10,10 @@ class Solution:
                 num = num // 10
             num = sum
         return num
+
+#python 3
+class Solution:
+    def addDigits(self, num: int) -> int:
+        if num == 0: return 0
+        if num % 9 == 0: return 9
+        else: return (num % 9)
