c7788d1a5e62f6699276fcd46b1c1be5d5a49664
Akash Reddy Jammula <jammulaakashreddy03@gmail.com>
2023-05-02 17:32:32 +0530

    Update 01052023Lt#1822. Sign of the Product of an Array.java

--- Leetcode-DailyChallenge/2K23_May/01052023Lt#1822. Sign of the Product of an Array.java
+++ Leetcode-DailyChallenge/2K23_May/01052023Lt#1822. Sign of the Product of an Array.java
@@ -16,3 +16,28 @@ class Solution {
     }
   
 }
+
+//Java
+class Solution {
+    public int arraySign(int[] nums) {
+        int product = 1;
+        for(int i = 0; i < nums.length; i++){ // n times
+            if (nums[i] > 0){
+                product *= 1;
+            } else if (nums[i] < 0){
+                product *= -1;
+            } else{
+                product *= 0;
+            }
+        }
+        return signFunc(product);
+    }
+    private int signFunc(int num){
+        if (num > 0){
+            return 1;
+        } else if(num < 0){
+            return -1;
+        } 
+        return 0;
+    }
+}
