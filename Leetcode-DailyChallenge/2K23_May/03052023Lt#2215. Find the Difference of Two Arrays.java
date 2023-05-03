0f6f5eb45ddd1ea59146ed6ca1a91087bb8159da
Akash Reddy Jammula <jammulaakashreddy03@gmail.com>
2023-05-03 13:02:11 +0530

    Update 03052023Lt#2215. Find the Difference of Two Arrays.java

--- Leetcode-DailyChallenge/2K23_May/03052023Lt#2215. Find the Difference of Two Arrays.java
+++ Leetcode-DailyChallenge/2K23_May/03052023Lt#2215. Find the Difference of Two Arrays.java
@@ -32,3 +32,29 @@ class Solution {
         return result;
     }
 }
+
+//Java
+class Solution {
+    public List<List<Integer>> findDifference(int[] nums1, int[] nums2) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        
+        Set<Integer> set1 = new HashSet<>();
+        Set<Integer> set2 = new HashSet<>();
+
+        for (int n: nums1){
+            set1.add(n);
+        }
+        for (int n: nums2){
+            set2.add(n);
+        }
+
+        List<Integer> ans1 = new ArrayList<>(set1);
+        List<Integer> ans2 = new ArrayList<>(set2);
+        ans1.removeAll(set2);
+        ans2.removeAll(set1);
+
+        result.add(ans1);
+        result.add(ans2);
+        return result;
+    }
+}
