a6f84d8083a68d0e99963570130be27b459e5928
Akash Reddy Jammula <jammulaakashreddy03@gmail.com>
2023-05-16 10:44:38 +0530

    Update 16052023Lt#24. Swap Nodes in Pairs.java

--- Leetcode-DailyChallenge/2K23_May/16052023Lt#24. Swap Nodes in Pairs.java
+++ Leetcode-DailyChallenge/2K23_May/16052023Lt#24. Swap Nodes in Pairs.java
@@ -1,5 +1,6 @@
 //24. Swap Nodes in Pairs
 
+#python3
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
@@ -15,3 +16,37 @@ class Solution:
             left.val, right.val = right.val, left.val
         
         return head
+
+//Java
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null){
+            return head;
+        }
+        
+        ListNode d = new ListNode(0), a;
+        d.next = head;
+        a = d;
+
+        while (head != null && head.next != null){
+            a.next = head.next;
+            head.next =  a.next.next;
+            a.next.next = head;
+
+            head = head.next;
+            a = a.next.next;
+        }
+
+        return d.next;
+    }
+}
