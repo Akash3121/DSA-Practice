52c9090ad43abec343af5a559c5e312c6898d56a
Akash Reddy Jammula <jammulaakashreddy03@gmail.com>
2023-05-18 11:25:07 +0530

    Update 18052023Lt#1557. Minimum Number of Vertices to Reach All Nodes.java

--- Leetcode-DailyChallenge/2K23_May/18052023Lt#1557. Minimum Number of Vertices to Reach All Nodes.java
+++ Leetcode-DailyChallenge/2K23_May/18052023Lt#1557. Minimum Number of Vertices to Reach All Nodes.java
@@ -24,3 +24,22 @@ class Solution {
         return result;
     }
 }
+
+//Java
+class Solution {
+    public List<Integer> findSmallestSetOfVertices(int n, List<List<Integer>> edges) {
+        int[] inDegree = new int[n];
+        for(List<Integer> edge : edges){
+            inDegree[edge.get(1)]++;
+        }
+
+        List<Integer> ans = new ArrayList<>();
+        for(int i = 0; i < n; i ++){
+            if (inDegree[i] == 0){
+                ans.add(i);
+            }
+        }
+
+        return ans;
+    }
+}
