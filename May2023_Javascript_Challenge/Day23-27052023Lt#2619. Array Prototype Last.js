96c547937d867f011ce14d698f74b32acdd11a0e
Akash Reddy Jammula <jammulaakashreddy03@gmail.com>
2023-05-27 14:46:27 +0530

    Create Day23-27052023Lt#2619. Array Prototype Last.js

---
+++ May2023_Javascript_Challenge/Day23-27052023Lt#2619. Array Prototype Last.js
@@ -0,0 +1,14 @@
+//2619. Array Prototype Last
+
+#1
+Array.prototype.last = function() {
+    if (this.length) {
+        return this[this.length - 1];
+    }
+    return -1;
+};
+
+/**
+ * const arr = [1, 2, 3];
+ * arr.last(); // 3
+ */
