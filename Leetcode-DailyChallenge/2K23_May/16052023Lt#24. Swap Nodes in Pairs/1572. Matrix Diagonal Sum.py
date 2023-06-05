cc52c0c27a685fdcce56975e9dd96d46d56d59d6
Akash Reddy Jammula <jammulaakashreddy03@gmail.com>
2023-06-05 12:50:06 +0530

    Create 1572. Matrix Diagonal Sum.py

---
+++ Leetcode-DailyChallenge/2K23_May/16052023Lt#24. Swap Nodes in Pairs/1572. Matrix Diagonal Sum.py
@@ -0,0 +1,13 @@
+# 1572. Matrix Diagonal Sum
+
+#python3
+class Solution:
+    def diagonalSum(self, mat: List[List[int]]) -> int:
+        s = 0
+        n = len(mat)
+        for i in range(n):
+            s += mat[i][i]
+            s += mat[i][n-i-1]
+        if n%2:
+            return s - mat[n//2][n//2]
+        return s
