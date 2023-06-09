373e2b1273e8afdcbea82339be3991775147daca
Akash Reddy Jammula <jammulaakashreddy03@gmail.com>
2023-06-09 08:53:04 +0530

    Create 1351. Count Negative Numbers in a Sorted Matrix.java

---
+++ Leetcode-DailyChallenge/2K23_June/08062023Lt#1351. Count Negative Numbers in a Sorted Matrix/1351. Count Negative Numbers in a Sorted Matrix.java
@@ -0,0 +1,25 @@
+//1351. Count Negative Numbers in a Sorted Matrix
+
+//Java
+class Solution {
+    public int countNegatives(int[][] grid) {
+        int mRow = grid.length;//4
+        int mCol = grid[0].length;//4
+
+        int ans = 0;
+
+        int row = 0;
+        int col = mCol - 1;
+        // n + m times, so O(n)
+        while(row < mRow && col < mCol && col > -1 && row > -1){
+            if (grid[row][col] < 0){ // col skipping
+                ans += mRow - row;
+                col--;
+            } else { //row skipping
+                row ++;
+            }
+        }
+        return ans;
+        
+    }
+}
